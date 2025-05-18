d68d070ffb0f09b3f9edcba3b8aab72ea6dfdba2
Danish nazir <danishnazir20699@gmail.com>
2025-05-18 14:55:22 +0530

    Initial commit with fruit shop functionality

--- demo.js
+++ demo.js
@@ -1,7 +1,13 @@
-console.log("This is a demo file");
-console.log("this is about git and github");
-console.log("and i am learning it now");
-console.log("this is a demo file");
+// console.log("This is a demo file");
+// console.log("this is about git and github");
+// console.log("and i am learning it now");
+// console.log("this is a demo file");
 
 
+// Write your code below:
+const fruitItem = document.getElementsByClassName("fruit");
+fruitItem[2].style.backgroundColor = 'yellow';
 
+for (let i = 0; i < fruitItem.length; i++){
+    fruitItem[i].style.fontWeight = 'bold';
+}
